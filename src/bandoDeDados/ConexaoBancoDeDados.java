d8bf7766c07e0a98d730ba6274c08f61ec29fe4b
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-28 18:45:09 -0400

    Initial commit

---
+++ src/bandoDeDados/ConexaoBancoDeDados.java
@@ -0,0 +1,22 @@
+package bandoDeDados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoBancoDeDados {
+
+    public static void main(String[] args) throws SQLException {
+
+        final String url = "jdbc:mysql://localhost:3306"; //Criando conexão com banco de dados
+        final String usuario = "root"; // criando senha do banco de dados
+        final String senha = "f3rr31r4"; // criando usuário do banco de dados
+
+        Connection conexao = DriverManager.getConnection(url, usuario, senha); //conectando no banco de dados
+        System.out.println("Conexão realizada com sucesso!");
+
+
+        conexao.close(); //fechando o conexao com banco de dados
+    }
+
+}
