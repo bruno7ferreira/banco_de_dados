9dca2e9d9977610963671bb17d0b6d04f5c183e1
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-28 23:07:51 -0400

    Configurando biblioteca para o jbdc

---
+++ src/bandoDeDados/CriarBanco.java
@@ -0,0 +1,26 @@
+package bandoDeDados;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CriarBanco {
+
+    public static void main(String[] args) {
+
+        final String url = "jdbc:mysql://localhost:3306/br";
+        final String usuario = "root";
+        final String senha = "f3rr31r4";
+
+        try {
+            Connection conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão ao banco de dados realizada com sucesso!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao conectar no Banco de Dados...");
+            System.out.println("tipo de erro: " + e.getMessage());
+
+        }
+
+    }
+}
