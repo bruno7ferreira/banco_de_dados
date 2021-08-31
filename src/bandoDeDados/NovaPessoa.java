0b57938bcc87451e1da53b7bb1bd0025612e272f
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-31 18:30:03 -0400

    Aprendendo a criar banco de dados no java.

--- src/bandoDeDados/NovaPessoa.java
+++ src/bandoDeDados/NovaPessoa.java
@@ -1,9 +1,21 @@
 package bandoDeDados;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class NovaPessoa {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe o nome: ");
+        String nome = entrada.next();
+
+        String sql = "insert into pessoas (nome) values('" + nome + "')";
+        System.out.println(sql);
 
 
+        entrada.close();
     }
 }
