401c4dfcbe1b47f29759a414970871788c893ff9
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-31 18:43:16 -0400

    Aprendendo a criar banco de dados no java.

--- src/bandoDeDados/NovaPessoa.java
+++ src/bandoDeDados/NovaPessoa.java
@@ -1,5 +1,7 @@
 package bandoDeDados;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -10,10 +12,13 @@ public class NovaPessoa {
         Scanner entrada = new Scanner(System.in);
 
         System.out.print("Informe o nome: ");
-        String nome = entrada.next();
+        String nome = entrada.nextLine();
 
-        String sql = "insert into pessoas (nome) values('" + nome + "')";
-        System.out.println(sql);
+        Connection conexao = FabricaConexao.getConexao(); //criando a conexão com banco de dados
+        String sql = "insert into pessoas (nome) values(?)"; //adicionando valor da variavel no banco de dados
+        PreparedStatement stmt = conexao.prepareStatement(sql);
+        stmt.setString(1, nome); //parametro de entrada da variavel para o sql
+        stmt.execute();
 
 
         entrada.close();
