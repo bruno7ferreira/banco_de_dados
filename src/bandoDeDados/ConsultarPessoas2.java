31db780bab88c8b5331cda051e1dc67956a6cdfc
Bruno Rogério <b.rogerio2007@gmail.com>
2021-08-31 22:08:36 -0400

    Aprendendo Query no banco de dados no java.

--- src/bandoDeDados/ConsultarPessoas2.java
+++ src/bandoDeDados/ConsultarPessoas2.java
@@ -1,8 +1,36 @@
 package bandoDeDados;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConsultarPessoas2 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+
+        //criar conexão
+        Connection conexao = FabricaConexao.getConexao();
+        String sql = "select * from pessoas";
+
+        Statement demonstracao = conexao.createStatement();
+        ResultSet resultado = demonstracao.executeQuery(sql);
+
+        List<Pessoa> pessoas = new ArrayList<>();
+        while (resultado.next()) {
+            int codigo = resultado.getInt("codigo");
+            String nome = resultado.getString("nome");
+            pessoas.add(new Pessoa(codigo, nome));
+        }
+        for (Pessoa p : pessoas) {
+            System.out.println(p.getCodigo() + " --> " + p.getNome());
+        }
+
+        demonstracao.close();
+        conexao.close();
+
 
     }
 }
