d46230584145d2190a0343a39ba768421c9b2af5
Bruno Rogério <b.rogerio2007@gmail.com>
2021-09-03 23:23:19 -0400

    Aprendendo Query no banco de dados no java.

--- src/bandoDeDados/AlterarNomePessoa.java
+++ src/bandoDeDados/AlterarNomePessoa.java
@@ -1,5 +1,30 @@
 package bandoDeDados;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class AlterarNomePessoa {
+
+    public static void main(String[] args) throws SQLException {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o código da Pessoa: ");
+        int codigo = entrada.nextInt();
+
+        String sql = "select * from pessoas where codigo = ?"; //Comando SQL
+        Connection conexao = FabricaConexao.getConexao();// criando conexao com banco de dados
+        PreparedStatement declaracaoPreparada = conexao.prepareStatement(sql);//preparação para receber linha de comando SQL
+        declaracaoPreparada.setInt(1, codigo);//tipo de dado recebido na declaração
+        ResultSet resultado = declaracaoPreparada.executeQuery();
+
+        if ()
+
+        conexao.close();
+        entrada.close();
+    }
 
 }
