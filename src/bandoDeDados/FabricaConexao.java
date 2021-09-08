ff5e1386dfc6599f7a508ed388265d0813e25993
Bruno Rogério <b.rogerio2007@gmail.com>
2021-09-07 20:41:37 -0400

    Aprendendo banco de dados no java.

--- src/bandoDeDados/FabricaConexao.java
+++ src/bandoDeDados/FabricaConexao.java
@@ -1,23 +1,37 @@
 package bandoDeDados;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
     public static Connection getConexao() {
 
         try {
-            final String url = "jdbc:mysql://localhost:3306/curso_java";
-            final String usuario = "root";
-            final String senha = "f3rr31r4";
+            Properties prop = getProperties();
+            final String url = prop.getProperty("banco.url");
+            final String usuario = prop.getProperty("banco.usuario");
+            final String senha = prop.getProperty("banco.senha");
+
+//            final String url = "jdbc:mysql://localhost:3306/curso_java"; //url do banco de dados
+//            final String usuario = "root"; // usuario do banco de dados
+//            final String senha = "f3rr31r4";// senha do banco de dados
 
             return DriverManager.getConnection(url, usuario, senha);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
+    private static Properties getProperties() throws IOException {
+        //criando acesso ao arquivo de configuraçoes
+        Properties prop = new Properties();
+        String caminho = "/conexao.properties";
+        prop.load(FabricaConexao.class.getResourceAsStream(caminho));
+        return prop;
+    }
 }
