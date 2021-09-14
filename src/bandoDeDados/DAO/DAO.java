11ed3779786a74a5a79e7a138c8194863c17dabe
Bruno Rogério <b.rogerio2007@gmail.com>
2021-09-13 22:18:38 -0400

    Aprendendo banco de dados no java.

---
+++ src/bandoDeDados/DAO/DAO.java
@@ -0,0 +1,16 @@
+package bandoDeDados.DAO;
+
+import java.sql.Connection;
+
+public class DAO {
+
+    private Connection conexao;
+
+    private Connection getConexao() {
+        if (conexao != null){
+
+        }
+
+    }
+
+}
