332aa14a95734231b0da4e9569f331079e094c82
Bruno Rogério <b.rogerio2007@gmail.com>
2021-12-15 23:40:57 -0400

    Aprendendo banco de dados no java.

---
+++ src/praticandoNovamente/FabricaConexao.java
@@ -0,0 +1,24 @@
+package praticandoNovamente;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+    public static Connection getConexao() {
+
+        try {
+            final String url = "jdbc:mysql://localhost:3306";
+            final String usuario = "root";
+            final String senha = "f3rr31r4";
+
+            return DriverManager.getConnection(url, usuario, senha);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+
+
+}
