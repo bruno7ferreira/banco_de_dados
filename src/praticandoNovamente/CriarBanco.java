c2793b5f904b805060c47a34cb3265a0d3b73878
Bruno Rogério <b.rogerio2007@gmail.com>
2021-12-21 22:34:33 -0400

    Aprendendo banco de dados no java.

--- src/praticandoNovamente/CriarBanco.java
+++ src/praticandoNovamente/CriarBanco.java
@@ -6,22 +6,18 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 
-
 public class CriarBanco {
 
     public static void main(String[] args) throws SQLException {
 
-        final String url = "jdbc:mysql://localhost:3306";
-        final String usuario = "root";
-        final String senha = "f3rr31r4";
 
-        Connection conexao1 = DriverManager.getConnection(url, usuario, senha);
+        Connection conexao = FabricaConexao.getConexao();
 
-        Statement stmt = conexao1.createStatement();
+        Statement stmt = conexao.createStatement();
         stmt.execute("create database if not exists curso_java14");
 
         System.out.println("Banco criado com sucesso!");
-        conexao1.close();
+        conexao.close();
 
 
     }
---
+++ src/praticandoNovamente/NovaPessoa.java
@@ -0,0 +1,27 @@
+package praticandoNovamente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class NovaPessoa {
+
+    public static void main(String[] args) throws SQLException {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe o nome: ");
+        String nome = entrada.nextLine();
+
+        Connection conexao = FabricaConexao.getConexao();
+
+        String sql = "insert into pessoas (nome) values(?)";
+        PreparedStatement declaracaoPreparada = conexao.prepareStatement(sql);
+
+
+        entrada.close();
+    }
+
+
+}
