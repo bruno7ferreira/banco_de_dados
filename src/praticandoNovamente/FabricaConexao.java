2266e0a5c808cab0e6467fafd997ef99f66d6d14
Bruno Rogério <b.rogerio2007@gmail.com>
2021-12-21 22:47:43 -0400

    Aprendendo banco de dados no java.

--- src/praticandoNovamente/FabricaConexao.java
+++ src/praticandoNovamente/FabricaConexao.java
@@ -9,7 +9,7 @@ public class FabricaConexao {
     public static Connection getConexao() {
 
         try {
-            final String url = "jdbc:mysql://localhost:3306";
+            final String url = "jdbc:mysql://localhost:3306/curso_java14";
             final String usuario = "root";
             final String senha = "f3rr31r4";
 
