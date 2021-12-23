d4d3edd7bf355e70a8edf34c9a6c761506db13f0
Bruno Rogério <b.rogerio2007@gmail.com>
2021-12-22 20:46:21 -0400

    Aprendendo banco de dados no java.

--- src/praticandoNovamente/ConsultarPessoa11.java
+++ src/praticandoNovamente/ConsultarPessoa11.java
@@ -2,7 +2,10 @@ package praticandoNovamente;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConsultarPessoa11 {
 
@@ -12,8 +15,21 @@ public class ConsultarPessoa11 {
 
         String sql = "select * from pessoas";
         PreparedStatement declaracaoPreparada = conexao.prepareStatement(sql);
-        declaracaoPreparada.execute(sql);
+        ResultSet resultado = declaracaoPreparada.executeQuery(sql);
 
+        List<Pessoa1> pessoas = new ArrayList<>();
+
+        while (resultado.next()) {
+            int codigo = resultado.getInt("codigo");
+            String nome = resultado.getString("nome");
+            pessoas.add(new Pessoa1(codigo, nome));
+        }
+
+        for (Pessoa1 p : pessoas) {
+            System.out.println(p.getCodigo() + " --> " + p.getNome());
+        }
+
+        declaracaoPreparada.close();
         conexao.close();
     }
 
