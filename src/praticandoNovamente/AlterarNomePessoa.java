f545aede04ac367d42202a24693606febd870a37
Bruno Rogério <b.rogerio2007@gmail.com>
2021-12-26 01:22:20 -0400

    Aprendendo banco de dados no java.

---
+++ src/praticandoNovamente/AlterarNomePessoa.java
@@ -0,0 +1,49 @@
+package praticandoNovamente;
+
+import bandoDeDados.Pessoa;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class AlterarNomePessoa {
+
+    public static void main(String[] args) throws SQLException {
+
+        Scanner entrada = new Scanner(System.in);
+        Connection conexao = FabricaConexao.getConexao();
+
+        System.out.println("Informe o código da pessoa: ");
+        int codigo = entrada.nextInt();
+
+        String select = "select  * from pessoas where ";
+        String update = "update pessoas set nome = ? where codigo = ?";
+        PreparedStatement declaraPreparada = conexao.prepareStatement(select);
+        declaraPreparada.setInt(1, codigo);
+        ResultSet resultado = declaraPreparada.executeQuery();
+
+        if (resultado.next()) {
+            Pessoa1 cont = new Pessoa1(resultado.getInt(1), resultado.getString(2));
+
+            System.out.println("O nome atual é " + cont.getNome());
+
+            System.out.println("Informe o novo nome: ");
+            String novoNome = entrada.nextLine();
+
+
+            declaraPreparada.close();
+            declaraPreparada = conexao.prepareStatement(update);
+            declaraPreparada.setString(1, novoNome);
+            declaraPreparada.setInt(2, codigo);
+
+            System.out.println("Pessoa alterada com sucesso!");
+        }
+
+
+        conexao.close();
+        entrada.close();
+    }
+
+}
