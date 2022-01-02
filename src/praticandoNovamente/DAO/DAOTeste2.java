8f38ea4f0e633c0efb4f559a6cf105bfcee71f1d
Bruno Rogério <b.rogerio2007@gmail.com>
2022-01-02 17:42:46 -0400

    Aprendendo banco de dados no java.

--- src/praticandoNovamente/DAO/DAOTeste2.java
+++ src/praticandoNovamente/DAO/DAOTeste2.java
@@ -1,14 +1,37 @@
 package praticandoNovamente.DAO;
 
+import java.util.Scanner;
+
 public class DAOTeste2 {
 
     public static void main(String[] args) {
 
+        Scanner entrada = new Scanner(System.in);
         DAO dao = new DAO();
 
-        String sql = "insert into pessoas (nome) values (?)";
-        dao.incluir(sql, "Bruno Rogério Ferreira");
+        //Menu
+        System.out.println("Selecione a opção desejada!");
+        System.out.println("1 - para Consultar \n" +
+                "2 - para adicionar \n" +
+                "3 - para remover \n");
+        String opcao = entrada.nextLine();
 
+        switch (opcao) {
+            case "1":
+                break;
+            case "2":
+                String sql = "insert into pessoas (nome) values (?)";
+                dao.incluir(sql, "Bruno Rogério Ferreira");
+                break;
+            case "3":
+                break;
+        }
+
+
+//        String sql = "insert into pessoas (nome) values (?)";
+//        dao.incluir(sql, "Bruno Rogério Ferreira");
+
+        entrada.close();
     }
 
 }
